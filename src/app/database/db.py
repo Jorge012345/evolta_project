3b09a07cdc2c03b91bc43649fe64353d827cf200
Jorge Terrazas <kakaroto_99_m@hotmail.com>
2024-06-20 22:13:36 -0500

    conexion db

--- src/app/database/db.py
+++ src/app/database/db.py
@@ -2,8 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# DATABASE_URL = "postgresql://devttecnico23:PLKIh2d83d$=@127.0.0.1/devttecnico23"
-DATABASE_URL = "postgresql://postgres:admin@localhost/evolta"
+DATABASE_URL = "postgresql://devttecnico23:PLKIh2d83d$=@127.0.0.1/devttecnico23"
+# DATABASE_URL = "postgresql://postgres:admin@localhost/evolta"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
